150832a8515c338bfdbf836583596e2443c97910
elx7 <77843884+elx7@users.noreply.github.com>
2021-02-24 19:29:52 +0100

    Adapting JUnit tests to new custom exception IncorrectItemForMenuList

--- F21AS/src/test/MenuListJUnitTests.java
+++ F21AS/src/test/MenuListJUnitTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import main.IncorrectItemForMenuList;
 import main.Item;
 import main.MenuList;
 
@@ -15,20 +16,20 @@ class MenuListJUnitTests {
 
 	@Test
 	@DisplayName("Testing the item's ID verification of the addToList method")
-	void testAddToListItemID() {
+	void testAddToListItemID() throws IncorrectItemForMenuList {
 		MenuList ml = new MenuList();
 		Item mi = new Item("FOOD", "Croissant", "French Nature Pastry.", 1, 3.5);
 		
-		Throwable exception = assertThrows(IllegalStateException.class, () -> ml.addToList(mi));
-		assertEquals("ItemID is too short.", exception.getMessage(), "Setting an ItemID too short should not work.");
+		Throwable exception = assertThrows(IncorrectItemForMenuList.class, () -> ml.addToList(mi));
+		assertEquals("Item ID is too short.", exception.getMessage(), "Setting an ItemID too short should not work.");
 		
 		Item mi2 = new Item("FOAD1", "Croissant", "French Nature Pastry.", 1, 3.5);
-		Throwable exceptionB = assertThrows(IllegalStateException.class, () -> ml.addToList(mi2));
-		assertEquals("ItemID category identifier incorrect.", exceptionB.getMessage(), "Setting a wrong ItemID category identifier should not work.");
+		Throwable exceptionB = assertThrows(IncorrectItemForMenuList.class, () -> ml.addToList(mi2));
+		assertEquals("Item ID category identifier incorrect.", exceptionB.getMessage(), "Setting a wrong ItemID category identifier should not work.");
 		
 		Item mi3 = new Item("FOODA", "Croissant", "French Nature Pastry.", 1, 3.5);
-		Throwable exceptionC = assertThrows(NumberFormatException.class, () -> ml.addToList(mi3));
-		assertEquals("ItemID doesn't have a number identifier.", exceptionC.getMessage(), "Setting no ItemID number identifier should not work.");
+		Throwable exceptionC = assertThrows(IncorrectItemForMenuList.class, () -> ml.addToList(mi3));
+		assertEquals("Item ID doesn't have a number identifier.", exceptionC.getMessage(), "Setting no ItemID number identifier should not work.");
 		
 		Item mi4 = new Item("FOOD365", "Croissant", "French Nature Pastry.", 1, 3.5);
 		ml.addToList(mi4);
@@ -37,11 +38,11 @@ class MenuListJUnitTests {
 
 	@Test
 	@DisplayName("Testing the item's Name verification of the addToList method")
-	void testAddToListItemName() {
+	void testAddToListItemName() throws IncorrectItemForMenuList {
 		MenuList ml = new MenuList();
 		Item mi = new Item("FOOD365", "", "French Nature Pastry.", 1, 3.5);
 		
-		Throwable exception = assertThrows(IllegalStateException.class, () -> ml.addToList(mi));
+		Throwable exception = assertThrows(IncorrectItemForMenuList.class, () -> ml.addToList(mi));
 		assertEquals("Item name blank.", exception.getMessage(), "Setting no Item name should not work.");
 		
 		Item mi2 = new Item("FOOD365", "Croissant", "French Nature Pastry.", 1, 3.5);
@@ -51,11 +52,11 @@ class MenuListJUnitTests {
 
 	@Test
 	@DisplayName("Testing the item's Price verification of the addToList method")
-	void testAddToListItemPrice() {
+	void testAddToListItemPrice() throws IncorrectItemForMenuList {
 		MenuList ml = new MenuList();
 		Item mi = new Item("FOOD445", "Croissant", "French Nature Pastry.", 1, -2.5);
 		
-		Throwable exception = assertThrows(IllegalStateException.class, () -> ml.addToList(mi));
+		Throwable exception = assertThrows(IncorrectItemForMenuList.class, () -> ml.addToList(mi));
 		assertEquals("Item price negative.", exception.getMessage(), "Setting a negative price should not work.");
 		
 		Item mi2 = new Item("FOOD365", "Croissant", "French Nature Pastry.", 1, 3.5);
@@ -65,7 +66,7 @@ class MenuListJUnitTests {
 
 	@Test
 	@DisplayName("Testing getMenuItem method")
-	void testgetMenuItem() {
+	void testgetMenuItem() throws IncorrectItemForMenuList {
 	    MenuList menuList = new MenuList();
 	    Item mi = new Item("FOOD445", "Croissant", "French Nature Pastry.", 1, 3.5);
 	    Item mi2 = new Item("BEVE445", "Water", "The original.", 1, 1.5);
