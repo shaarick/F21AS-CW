d7d1dc6cb780ef571e694c59c310280872bb9f85
wam4 <andrew_manson@aol.com>
2021-02-27 12:01:42 +0000

    Comments

--- F21AS/src/main/Item.java
+++ F21AS/src/main/Item.java
@@ -1,6 +1,7 @@
 package main;
 
 public class Item {
+	//Fields
 	private String itemID;
 	private String name;
 	private String description;
@@ -9,51 +10,65 @@ public class Item {
 	private double priceTotal;
 	
 	
+	//Constructor
 	public Item(String itemID, String name, String description, int quantity, double price) {
 		try {
 			setItemID(itemID);
-			setItemName(name);
-			setItemDescription(description);
-			setItemQuantity(quantity);
-			setItemPrice(price);
 		}
 		catch(StringLengthException exception) {
 			System.out.println(exception.getMessage());
 		}
+		try {
+			setItemName(name);
+		}
+		catch(StringLengthException exception) {
+			System.out.println(exception.getMessage());
+		}
+		try {
+			setItemDescription(description);
+		}
+		catch(StringLengthException exception) {
+			System.out.println(exception.getMessage());
+		}
+		setItemQuantity(quantity);
+		setItemPrice(price);
 	}
 
-
+	
+	//Get methods
+	//Get ID of item
 	public String getItemID() {
 		return itemID;
 	}
-
-
+	
+	//Get name of item
 	public String getItemName() {
 		return name;
 	}
 	
-	
+	//Get description of item
 	public String getItemDescription() {
 		return description;
 	}
-
-
+	
+	//Get quantity of item
 	public int getItemQuantity() {
 		return quantity;
 	}
-
-
+	
+	//Get price of item
 	public double getItemPrice() {
 		return price;
 	}
-
-
+	
+	//Get total price of item
 	public double getItemPriceTotal() {
 		return priceTotal;
 	}
 	
 	
-	
+	//Set methods
+	//Set ID of item with limit of 7 characters
 	public void setItemID(String itemID) throws StringLengthException {
 		int maxSize = 7;
 		if(itemID.length() <= maxSize) {
@@ -63,8 +78,8 @@ public class Item {
 			throw new StringLengthException("ID",maxSize);
 		}
 	}
-
-
+	
+	//Set name of item with limit of 16 characters
 	public void setItemName(String name) throws StringLengthException {
 		int maxSize = 16;
 		if(name.length() <= maxSize) {
@@ -75,7 +90,7 @@ public class Item {
 		}
 	}
 	
-	
+	//Set description of item with limit of 25 characters
 	public void setItemDescription(String description) throws StringLengthException {
 		int maxSize = 25;
 		if(description.length() <= maxSize) {
@@ -85,8 +100,8 @@ public class Item {
 			throw new StringLengthException("Description",maxSize);
 		}
 	}
-
-
+	
+	//Set quantity of item as absolute value
 	public void setItemQuantity(int quantity) {
 		if(quantity>=0) {
 			this.quantity = quantity;
@@ -96,8 +111,8 @@ public class Item {
 		}
 		setItemPriceTotal();
 	}
-
-
+	
+	//Set price of item as absolute value
 	public void setItemPrice(double price) {
 		if(price>=0) {
 			this.price = price;
@@ -107,37 +122,37 @@ public class Item {
 		}
 		setItemPriceTotal();
 	}
-
-
+	
+	//Set price total of item as product of price and quantity of item
 	public void setItemPriceTotal() {
 		priceTotal = price*quantity;
 	}
 	
 	
-	
-
+	//Other methods
+	//Increment quantity and total price of item
 	public void addOne() {
 		quantity++;
 		priceTotal+=price;
 	}
-
-
+	
+	//Decrement quantity and total price of item
 	public void substractOne() {
 		quantity--;
 		priceTotal-=price;
 	}
 	
-	
+	//Compare one item to another using item IDs
 	public int compareTo(Item item) {
 		return this.itemID.compareTo(item.getItemID());
 	}
 	
-	
+	//Check if two items are equal using item IDs
 	public boolean equals(Item item) {
 		return this.itemID.equals(item.getItemID());
 	}
 	
-	
+	//Find hash code of item using item ID
 	public int hashCode() {
 		return this.itemID.hashCode();
 	}
